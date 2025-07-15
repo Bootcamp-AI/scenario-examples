33de4d96ee56734230d5e6d7b5dfe5481881d6dd
BootcampAI <57165250+Bootcamp-AI@users.noreply.github.com>
2025-07-14 20:58:57 -0500

    Update node

--- dockerNode/background.sh
+++ dockerNode/background.sh
@@ -8,13 +8,14 @@ echo starting... # to test stdout output in /var/log/killercoda
 
 #touch /tmp/finished
 #cat my-file.txt
-git clone https://github.com/Bootcamp-AI/nodejs-app-starting-setup.git
 
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
 source ~/.bashrc
 nvm install 20.18.0
 nvm use 20.18.0
+
+git clone https://github.com/Bootcamp-AI/nodejs-app-starting-setup.git
 
 cd nodejs-app-starting-setup
 cat server.js
